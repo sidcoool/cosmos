f7552c1eb5bfbfa594b8ed1e5658f790c75d000c
sidcoool <32618581+sidcoool@users.noreply.github.com>
2017-10-09 22:14:03 +0530

    This is C program for finding the second largest number in an array

--- code/randomized-algorithms/shuffle_an_array/shuffle_an_array.cpp
+++ code/randomized-algorithms/shuffle_an_array/shuffle_an_array.cpp
@@ -1,27 +1,40 @@
-#include <array>
-#include <iostream>
-#include <iterator>
-#include <random>
+#include<stdio.h>
+int main()
+{
+ int a[101],i,n,pos,lar;
 
-// Part of Cosmos by OpenGenus Foundation
-template<class It, class RNG>
-void shuffle_an_array(It first, It last, RNG &&rng) {
-    std::uniform_int_distribution<> dist;
-    using ptype = std::uniform_int_distribution<>::param_type;
-    using std::swap;
-    
-    for (; first != last; ++first) {
-        swap(*first, *(first + dist(rng, ptype(0, last - first - 1))));
-    }
+ printf("Enter the value of n not more than 100\n");
+ scanf("%d",&n);
+
+ printf("Enter n numbers \n");
+
+ for(i=0;i<n;i++)
+ scanf("%d",&a[i]);
+
+ lar=a[0];
+ for(i=0;i<n;i++)
+ {
+     if(a[i]>lar)
+     {
+       lar=a[i];
+        pos=i;
+     }
+
+ }
+
+ for(i=pos;i<n-1;i++)
+a[i]=a[i+1];
+
+lar=a[0];
+for(i=0;i<n-1;i++)
+ {
+     if(a[i]>lar)
+     {
+       lar=a[i];
+     }
+
+ }
+ printf("The second largest number is %d",lar);
+    return 0;
 }
 
-int main(int argc, char *argv[]) {
-    std::array<int, 10> values{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    std::minstd_rand rng(std::random_device{}());
-    shuffle_an_array(values.begin(), values.end(), rng);
-    
-    for (int i=0; i < values.size(); ++i) {
-        std::cout << (i > 0 ? ", " : "") << values[i];
-    }
-    std::cout << std::endl;
-}
